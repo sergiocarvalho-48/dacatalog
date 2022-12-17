50107a669a930aa2a8c3943e090ca7a40c445122
Sergio Carvalho <srcarvalho52@gmail.com>
2022-12-17 12:20:26 -0300

    Props ProductCard, ProductPrice

--- frontweb/src/components/ProductPrice/index.tsx
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -1,12 +1,15 @@
 import './styles.css'
 
+type Props = {
+    price: number;
+}
 
-const ProductPrice = () => {
+const ProductPrice = ( { price } : Props) => {
 
     return (
         <div className="product-price-container">
             <span>R$</span>
-            <h3>2345.67</h3>
+            <h3>{price}</h3>
 
         </div>
     );
