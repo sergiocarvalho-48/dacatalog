c481871c96860c7d081f4bdba5d270106a383a25
Sergio Carvalho <srcarvalho52@gmail.com>
2023-01-21 14:27:49 -0300

    Delete product, observer pattern

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -1,15 +1,35 @@
+import { AxiosRequestConfig } from 'axios';
 import ProductPrice from 'components/ProductPrice';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
+import { requestBackend } from 'util/requests';
 import CategoryBadge from '../CategoryBadge';
 
 import './styles.css';
 
 type Props = {
   product: Product;
+  onDelete: Function;
 };
 
-const ProductCrudCard = ({ product }: Props) => {
+const ProductCrudCard = ({ product, onDelete }: Props) => {
+
+  const handleDelete = (productId: number) => {
+
+    if (!window.confirm("Tem certeza que deseja deletar?")) {
+      return;
+    }
+    const config: AxiosRequestConfig = {
+      method: 'DELETE',
+      url: `/products/${productId}`,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then(() => {
+      onDelete();
+    });
+  };
+
   return (
     <div className="base-card product-crud-card">
       <div className="product-crud-card-top-container">
@@ -27,15 +47,17 @@ const ProductCrudCard = ({ product }: Props) => {
         </div>
       </div>
       <div className="product-crud-card-buttons-container">
-            <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
-                EXCLUIR
-            </button>
-            <Link to={`/admin/products/${product.id}`}>
-            <button className="btn btn-outline-secondary product-crud-card-button">
-                EDITAR
-            </button>
-            </Link>
-            
+        <button
+          onClick={() => handleDelete(product.id)}
+          className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first"
+        >
+          EXCLUIR
+        </button>
+        <Link to={`/admin/products/${product.id}`}>
+          <button className="btn btn-outline-secondary product-crud-card-button">
+            EDITAR
+          </button>
+        </Link>
       </div>
     </div>
   );
