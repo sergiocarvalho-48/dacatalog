2aa948adec8b5e47a7123476a58583ba1179aa26
Sergio Carvalho <srcarvalho52@gmail.com>
2023-01-21 09:08:43 -0300

    Product crud routes

---
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -0,0 +1,10 @@
+
+
+const Form = () => {
+
+    return (
+        <h1>Tela form</h1>
+    )
+}
+
+export default Form;
---
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -0,0 +1,10 @@
+
+
+const List = () => {
+
+    return (
+        <h1>Tela list</h1>
+    )
+}
+
+export default List;
